1738991f8a5517dc6e333b135c66df1875b568f7
Vitor Rafael Marcelino <111241590+vitorrmarcelino@users.noreply.github.com>
2023-07-06 09:57:39 -0300

    alguns ajustes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
-    var scrollLinks = document.querySelectorAll('.scroll');
-  
-    // Adiciona um evento de clique a cada elemento
-    scrollLinks.forEach(function(link) {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        var target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-      });
+  // Aguarda o carregamento completo da página antes de executar o código
+
+  var scrollLinks = document.querySelectorAll('.scroll');
+  // Seleciona todos os elementos com a classe 'scroll' e os armazena em uma lista
+
+  // Adiciona um evento de clique a cada elemento da lista
+  scrollLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      // Captura o evento de clique em cada elemento
+
+      e.preventDefault();
+      // Prevenção do comportamento padrão do evento de clique, que é seguir o link e recarregar a página
+
+      var target = document.querySelector(link.getAttribute('href'));
+      // Obtém o valor do atributo 'href' do elemento clicado e encontra o elemento correspondente na página
+
+      target.scrollIntoView({ behavior: 'smooth' });
+      // Faz com que a página role suavemente até o elemento alvo encontrado, usando um comportamento de rolagem suave
     });
   });
-  
+});
